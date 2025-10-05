4be562bd70cd3a2e502e1faa9fed3dce163a31ac
Jaxon Ross <jaxonlucasross@usf.edu>
2025-10-05 15:27:30 -0400

    first commit

---
+++ cc_6.js
@@ -0,0 +1,10 @@
+// Step 1 
+class Employee {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+  describe() {
+    return `${this.name} works in ${this.department}.`;
+  }
+}
